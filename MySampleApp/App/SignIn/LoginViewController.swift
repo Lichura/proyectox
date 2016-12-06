685d43ac17294c0742faf2f923f41be824194976
Lisandro Gurman <lichun88@gmail.com>
2016-12-06 15:51:50 +0100

    Login scren 06dic2016

---
+++ MySampleApp/App/SignIn/LoginViewController.swift
@@ -0,0 +1,44 @@
+//
+//  LoginViewController.swift
+//  MySampleApp
+//
+//  Created by Lisandro Gurman on 12/6/16.
+//
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var crearNuevoUsuario: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        crearNuevoUsuario.addTarget(self, action: #selector(handleUserPoolSignUp), forControlEvents: .TouchUpInside)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func handleUserPoolSignUp () {
+        let storyboard = UIStoryboard(name: "UserPools", bundle: nil)
+        let viewController = storyboard.instantiateViewControllerWithIdentifier("SignUp")
+        self.presentViewController(viewController, animated:true, completion:nil);
+    }
+
+}
